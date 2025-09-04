4d75e8fcd06240ccb89fd3acef6d4e482b2a26f5
Lark Aster <lark@lark.gay>
2025-09-04 14:35:56 -0400

    Register node

---
+++ overstock/init.lua
@@ -0,0 +1,18 @@
+core.register_node("overstock:barrel", {
+	description = "Barrel",
+	tiles = { "overstock_barrel_front.png" },
+	groups = {
+		axey = 1,
+		material_wood = 1,
+	},
+})
+
+core.register_craft({
+	type = "shaped",
+	output = "overstock:barrel 1",
+	recipe = {
+		{ "group:tree", "group:wood_slab", "group:tree" },
+		{ "group:tree", "mcl_chests:chest", "group:tree" },
+		{ "group:tree", "group:tree", "group:tree" },
+	},
+})
